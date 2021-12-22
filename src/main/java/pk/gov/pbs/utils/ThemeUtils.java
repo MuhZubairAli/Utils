eec1da7024062eea49a7314894d90446c9241d3e
Muhammad Zubair <muhzubairali@gmail.com>
2021-12-22 23:11:14 +0500

    support for urdu fonts added, also Nastaleeq font added in assets

--- src/main/java/pk/gov/pbs/utils/ThemeUtils.java
+++ src/main/java/pk/gov/pbs/utils/ThemeUtils.java
@@ -3,13 +3,46 @@ package pk.gov.pbs.utils;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.ColorInt;
 
+import java.util.Locale;
+
 public class ThemeUtils {
+    protected static Typeface mUrduFontFace;
+
+    public static Typeface getUrduFontTypeFace(Context context) {
+        if (mUrduFontFace == null)
+            mUrduFontFace = Typeface.createFromAsset(context.getAssets(),
+                    "fonts/Jameel_Noori_Nastaleeq_Kasheeda.ttf");
+        return mUrduFontFace;
+    }
+
+    /**
+     * setup text style based on locale like Font, Size (theme independent(
+     * @param locale locale of the app, specified in LabelProvider constructor
+     * @param textViews subjects to which styles are to be applied
+     */
+    public static void setupTextViewStylesByLocale(Locale locale, TextView... textViews){
+        if (textViews.length > 0) {
+            if (locale.getLanguage().equalsIgnoreCase("ur"))
+                ThemeUtils.applyUrduFontTypeFace(textViews);
+        }
+    }
+
+    public static void applyUrduFontTypeFace(TextView... textViews){
+        if (mUrduFontFace == null)
+            getUrduFontTypeFace(textViews[0].getContext());
+
+        for (TextView tv : textViews)
+            tv.setTypeface(mUrduFontFace);
+    }
+
     public static void applyThemedDrawableToView(View view, int resId){
         Context context = view.getContext();
         TypedArray a = context.getTheme().obtainStyledAttributes(new int[] {resId});
